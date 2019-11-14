f3d374deddbdf0db369b1a34f259153e62753bce
csauques <csauques@scinfe059.univ-pau.fr>
2019-11-14 13:22:27 +0100

    adding fxml and deleting CSS file

--- AppliHandball/src/application/Main.java
+++ AppliHandball/src/application/Main.java
@@ -1,20 +1,33 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	static Stage stage1;
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Parent root = FXMLLoader.load(getClass().getResource("direct.fxml"));
+	        
+	        Scene scene = new Scene(root);
+	        
+	        stage1 = stage;
+	        
+	        stage.setScene(scene);
+	        stage.setTitle("Temps réel");
+	        stage.show();
+	        
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
